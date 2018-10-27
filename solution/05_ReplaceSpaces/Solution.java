0123a3bc07cc6444a68f027c8c94d8c8d27628cc
yanglbme <szuyanglb@outlook.com>
2018-10-27 23:13:44 +0800

    perf(solution): code change to improve performance
    Change code of solution 05 to improve performance

--- solution/05_ReplaceSpaces/Solution.java
+++ solution/05_ReplaceSpaces/Solution.java
@@ -6,7 +6,6 @@
 public class Solution {
     /**
      * 将字符串中的所有空格替换为%20
-     * 
      * @param str 字符串
      * @return 替换后的字符串
      */
@@ -14,13 +13,33 @@ public class Solution {
         if (str == null || str.length() == 0) {
             return str.toString();
         }
-        StringBuilder sb = new StringBuilder();
+        
         int len = str.length();
         for (int i = 0; i < len; ++i) {
-            char ch = str.charAt(i);
-            sb.append(ch == ' ' ? "%20" : ch);
+            if (str.charAt(i) == ' ') {
+                // append 两个空格
+                str.append("  ");
+            }
         }
 
-        return sb.toString();
+        // p 指向原字符串末尾
+        int p = len - 1;
+
+        // q 指向现字符串末尾
+        int q = str.length() - 1;
+
+        while (p >= 0) {
+            char ch = str.charAt(p--);
+            if (ch == ' ') {
+                str.setCharAt(q--, '0');
+                str.setCharAt(q--, '2');
+                str.setCharAt(q--, '%');
+            } else {
+                str.setCharAt(q--, ch);
+            }
+        }
+
+        return str.toString();
+
     }
 }
